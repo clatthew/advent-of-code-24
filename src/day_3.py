f3a6a6ee405f451c30c9cae0250c4ca35e1c5360
Matt Clough <mattjamesclough@gmail.com>
2024-12-03 14:40:24 +0000

    complete 3.2

--- src/day_3.py
+++ src/day_3.py
@@ -16,3 +16,27 @@ def task_1(corrupted_data=get_input_string()):
     for a, b in extract_muls(corrupted_data):
         total += int(a) * int(b)
     return total
+
+
+def extract_muls_2(corrupted_data):
+    pattern = compile(r"mul\(\d+,\d+\)|do\(\)|don't\(\)")
+    return pattern.findall(corrupted_data)
+
+
+def task_2(corrupted_data=get_input_string()):
+    mul = compile(r"mul\((\d+),(\d+)\)")
+    do = compile(r"do\(\)")
+    dont = compile(r"don't\(\)")
+    doing = True
+    total = 0
+    for instruction in extract_muls_2(corrupted_data):
+        if do.match(instruction):
+            doing = True
+        elif dont.match(instruction):
+            doing = False
+        if not doing:
+            continue
+        if mul.match(instruction):
+            a, b = mul.findall(instruction)[0]
+            total += int(a) * int(b)
+    return total
--- test/test_3.py
+++ test/test_3.py
@@ -1,5 +1,9 @@
-from src.day_3 import task_1
+from src.day_3 import task_1, task_2
 
 
 def test_1():
     assert task_1() == 163931492
+
+
+def test_2():
+    assert task_2() == 76911921
