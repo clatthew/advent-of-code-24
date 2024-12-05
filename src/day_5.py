42c8fd496bd19af7ca3660ed81c71172cf8df3c5
Matt Clough <mattjamesclough@gmail.com>
2024-12-05 15:32:02 +0000

    add type hints

--- src/day_5.py
+++ src/day_5.py
@@ -1,16 +1,16 @@
-def file_generator(filepath):
+def file_generator(filepath: str):
     with open(filepath) as f:
         yield from f
 
 
-def add_to_dict(dict, key, val):
+def add_to_dict(dict: dict, key, val):
     if key not in dict:
         dict[key] = [val]
     else:
         dict[key].append(val)
 
 
-def extract_input(filepath="input/5.txt"):
+def extract_input(filepath: str = "input/5.txt"):
     input = file_generator(filepath)
     conditions = {}
     updates = []
@@ -32,7 +32,9 @@ def get_badly_ordered_updates():
     return conditions, badly_ordered_updates
 
 
-def get_check_update_fail_return_val(rearrange, update, i, condition):
+def get_check_update_fail_return_val(
+    rearrange: bool, update: list, i: int, condition: int
+):
     if rearrange:
         update.remove(condition)
         update.insert(i, condition)
@@ -41,25 +43,25 @@ def get_check_update_fail_return_val(rearrange, update, i, condition):
         return 0
 
 
-def middle_val(update):
+def get_middle_val(update: list):
     return update[int(len(update) / 2)]
 
 
-def check_update(update, conditions, rearrange=False):
+def check_update(update: list, conditions: dict, rearrange: bool = False):
     for i, page in enumerate(update):
         for condition in conditions[page]:
             if condition in update and condition not in update[:i]:
                 return get_check_update_fail_return_val(rearrange, update, i, condition)
-    return middle_val(update)
+    return get_middle_val(update)
 
 
 def fix_update(update: list, conditions: dict):
     while not check_update(update, conditions):
         update = check_update(update, conditions, True)
-    return middle_val(update)
+    return get_middle_val(update)
 
 
-def tasks(input_func=extract_input, score_func=check_update):
+def tasks(input_func=extract_input, score_func=check_update) -> int:
     conditions, updates = input_func()
     total = 0
     for update in updates:
