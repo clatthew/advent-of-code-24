ffdb50235abfd17bf4e05e5bfa34092650f95ae8
Matt Clough <mattjamesclough@gmail.com>
2024-12-06 12:42:18 +0000

    add __str__ method to vector

--- src/day_4.py
+++ src/day_4.py
@@ -9,6 +9,9 @@ class Vector:
     def __sub__(self, other):
         return Vector(self.x - other.x, self.y - other.y)
 
+    def __str__(self):
+        return f"[{self.x}, {self.y}]"
+
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
 
