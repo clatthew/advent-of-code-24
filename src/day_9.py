339e8f47adc674f61702b5883b54119d0daae551
Matt Clough <mattjamesclough@gmail.com>
2024-12-09 19:02:27 +0000

    complete 9.2

--- src/day_9.py
+++ src/day_9.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 def input_string(filepath: str):
     with open(filepath) as f:
         return f.read()
@@ -11,41 +14,113 @@ def empty_chunks(data: str) -> list[int]:
     return [int(i) for i in data[1::2]] + [0]
 
 
-def uncompressed_rep(data: list[int]) -> str:
+def uncompressed_rep(
+    data: list[int], nested: bool = False
+) -> list[int] | list[Union[list[int], int]]:
     uncompressed_data = []
     for i, (data_len, empty_len) in enumerate(
         zip(data_chunks(data), empty_chunks(data))
     ):
-        uncompressed_data += [i] * data_len
-        uncompressed_data += [None] * empty_len
+        next_data_chunk = [i] * data_len
+        if nested:
+            uncompressed_data.append(next_data_chunk)
+            uncompressed_data.append(empty_len)
+        else:
+            next_empty_chunk = [None] * empty_len
+            uncompressed_data += next_data_chunk + next_empty_chunk
     return uncompressed_data
 
 
-def compressed_rep(uncompressed_data: str) -> str:
+def compressed_rep_1(uncompressed_data: list[int]) -> list[int]:
     compressed_data = []
     end_pointer = len(uncompressed_data) - 1
     for i, chunk in enumerate(uncompressed_data):
-        if i >= end_pointer:
-            return compressed_data
         if chunk is None:
             while uncompressed_data[end_pointer] is None:
-                end_pointer -= 1
                 if i == end_pointer:
                     return compressed_data
+                end_pointer -= 1
             compressed_data.append(uncompressed_data[end_pointer])
             end_pointer -= 1
         else:
             compressed_data.append(chunk)
+            if i >= end_pointer:
+                return compressed_data
 
 
 def generate_checksum(data: list[int]) -> int:
-    return sum([i * chunk for i, chunk in enumerate(data)])
+    return sum([i * chunk for i, chunk in enumerate(data) if chunk is not None])
 
 
-def task_1(filepath="input/9.txt") -> int:
+def task_1(filepath: str = "input/9.txt") -> int:
     data = input_string(filepath)
     uncompressed_data = uncompressed_rep(data)
-    with open("test/test_data/9_uncompressed.txt", "w") as f:
-        f.write("".join([str(i) for i in uncompressed_data]))
-    compressed_data = compressed_rep(uncompressed_data)
+    compressed_data = compressed_rep_1(uncompressed_data)
+    return generate_checksum(compressed_data)
+
+
+def find_gap_to_fit(
+    chunk: list[int], uncompressed_data: list[Union[list[int], int]]
+) -> int | None:
+    for i, gap_size in enumerate(uncompressed_data):
+        if isinstance(gap_size, int):
+            if len(chunk) <= gap_size:
+                return i
+    return None
+
+
+def merge_adjacents(
+    uncompressed_data: list[Union[list[int], int]],
+) -> list[Union[list[int], int]]:
+    for i, (gap_1, gap_2) in enumerate(
+        zip(uncompressed_data[:-1], uncompressed_data[1:])
+    ):
+        if isinstance(gap_1, int) and isinstance(gap_2, int):
+            uncompressed_data[i] += gap_2
+            uncompressed_data[i + 1] = 0
+    return [i for i in uncompressed_data if i]
+
+
+def make_optimisation(
+    old_chunk_pos: int,
+    candidate_chunk: list[int],
+    uncompressed_data: list[Union[list[int], int]],
+) -> list[Union[list[int], int]]:
+    gap_pos = find_gap_to_fit(candidate_chunk, uncompressed_data)
+    if gap_pos is None or gap_pos > old_chunk_pos:
+        return uncompressed_data
+    uncompressed_data[old_chunk_pos] = len(candidate_chunk)
+    uncompressed_data.insert(gap_pos, candidate_chunk)
+    new_empty_len = uncompressed_data[gap_pos + 1] - len(candidate_chunk)
+    if new_empty_len:
+        uncompressed_data[gap_pos + 1] = new_empty_len
+    else:
+        del uncompressed_data[gap_pos + 1]
+    while True:
+        compressed_data = merge_adjacents(uncompressed_data)
+        if compressed_data == merge_adjacents(compressed_data):
+            return compressed_data
+
+
+def compressed_rep_2(uncompressed_data: list[Union[list[int], int]]) -> list[int]:
+    for backward_chunk in uncompressed_data[::-1]:
+        if isinstance(backward_chunk, list):
+            uncompressed_data = make_optimisation(
+                uncompressed_data.index(backward_chunk),
+                backward_chunk,
+                uncompressed_data,
+            )
+    compressed_data = []
+    for chunk in uncompressed_data:
+        if isinstance(chunk, int):
+            compressed_data += [0] * chunk
+        else:
+            compressed_data += chunk
+    return compressed_data
+
+
+def task_2(filepath: str = "input/9.txt") -> int:
+    data = input_string(filepath)
+    uncompressed_data = uncompressed_rep(data, True)
+    compressed_data = compressed_rep_2(uncompressed_data)
     return generate_checksum(compressed_data)
--- test/test_9.py
+++ test/test_9.py
@@ -5,8 +5,10 @@ from src.day_9 import (
     empty_chunks,
     uncompressed_rep,
     task_1,
-    compressed_rep,
+    compressed_rep_1,
     generate_checksum,
+    task_2,
+    find_gap_to_fit,
 )
 
 
@@ -76,8 +78,35 @@ class Testuncompressed_rep:
         result = uncompressed_rep(input_string("test/test_data/9.txt"))
         assert result == expected
 
+    @mark.it("produces uncompressed rep of sample data with nesting")
+    def test_2(self):
+        expected = [
+            [0, 0],
+            3,
+            [1, 1, 1],
+            3,
+            [2],
+            3,
+            [3, 3, 3],
+            1,
+            [4, 4],
+            1,
+            [5, 5, 5, 5],
+            1,
+            [6, 6, 6, 6],
+            1,
+            [7, 7, 7],
+            1,
+            [8, 8, 8, 8],
+            0,
+            [9, 9],
+            0,
+        ]
+        result = uncompressed_rep(input_string("test/test_data/9.txt"), True)
+        assert result == expected
 
-class Testcompressed_rep:
+
+class Testcompressed_rep_1:
     @mark.it("produces compressed rep of sample data")
     def test_1(self):
         uncompressed_data = uncompressed_rep(input_string("test/test_data/9.txt"))
@@ -111,13 +140,22 @@ class Testcompressed_rep:
             6,
             6,
         ]
-        result = compressed_rep(uncompressed_data)
+        result = compressed_rep_1(uncompressed_data)
+        assert result == expected
+
+
+class Testfind_gap_to_fit:
+    def test_1(self):
+        uncompressed_data = uncompressed_rep(input_string("test/test_data/9.txt"), True)
+        expected = 1
+        result = find_gap_to_fit([9, 9], uncompressed_data)
         assert result == expected
 
 
 class Testgenerate_checksum:
+    @mark.it("produces checksum of sample data")
     def test_1(self):
-        compressed_data = compressed_rep(
+        compressed_data = compressed_rep_1(
             uncompressed_rep(input_string("test/test_data/9.txt"))
         )
         expected = 1928
@@ -130,4 +168,17 @@ class Testtasks:
     def test_2(self):
         expected = 6398608069280
         result = task_1()
+        assert result == expected
+
+    # @mark.skip
+    @mark.it("correct answer to task 2 with sample data")
+    def test_3(self):
+        expected = 2858
+        result = task_2("test/test_data/9.txt")
+        assert result == expected
+
+    @mark.it("correct answer to task 2")
+    def test_4(self):
+        expected = 6427437134372
+        result = task_2()
         assert result == expected
